4a952651f8e7c7c40bf32792f249f02038e9a84f
jozee <buxiaosheng123@gmail.com>
2018-07-28 16:34:46 +0800

    add ch18,ch19

--- utils.py
+++ utils.py
@@ -6,3 +6,22 @@ def show_train_history(train_history, train, validation):
     plt.xlabel('Epoch')
     plt.legend(['train','validation'], loc='upper left')
     plt.show()
+
+    
+def plot_images_labels_prediction(images, labels, prediction, idx, num=10):
+    fig = plt.gcf()
+    fig.set_size_inches(12,14)
+    if num>25: num=25
+    
+    for i in range(0, num):
+        ax = plt.subplot(5,5, 1+i)
+        ax.imshow(np.reshape(images[idx], (28,28)), cmap='binary')
+        title = 'label='+str(np.argmax(labels[idx]))
+        
+        if len(prediction)>0:
+            title +=", predict="+str(prediction[idx])
+            
+        ax.set_title(title, fontsize=10)
+        ax.set_xticks([]);ax.set_yticks([])
+        idx+=1
+    plt.show()
